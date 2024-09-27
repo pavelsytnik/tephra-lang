250983ea0a5bcebed4bcf537c8e6959f870d894b
Pavel Sytník <pashasytnik@gmail.com>
2024-09-27 23:47:33 +0400

    Refactor Expression.hpp

--- Expression.hpp
+++ Expression.hpp
@@ -3,71 +3,68 @@
 
 #include <memory>
 #include <utility>
+#include <variant>
 
 #include "Token.hpp"
 
 namespace tephra
 {
-class Expression
-{
-public:
-    virtual ~Expression() = 0;
-};
+struct BinaryExpr;
+struct GroupingExpr;
+struct LiteralExpr;
+struct UnaryExpr;
 
-class BinaryExpression : public Expression
-{
-public:
-    BinaryExpression(std::unique_ptr<Expression> left,
-                     Token&& op,
-                     std::unique_ptr<Expression> right
-    ) :
-        left(std::move(left)),
-        op(std::move(op)),
-        right(std::move(right))
-    {}
+using Expr = std::variant<
+    BinaryExpr,
+    GroupingExpr,
+    LiteralExpr,
+    UnaryExpr
+>;
 
-    const std::unique_ptr<Expression> left;
+struct BinaryExpr {
+    const std::unique_ptr<Expr> left;
     const Token op;
-    const std::unique_ptr<Expression> right;
+    const std::unique_ptr<Expr> right;
+
+    BinaryExpr(std::unique_ptr<Expr> left,
+               Token&& op,
+               std::unique_ptr<Expr> right)
+    :
+        left{std::move(left)},
+        op{std::move(op)},
+        right{std::move(right)}
+    {}
 };
 
-class GroupingExpression : public Expression
-{
-public:
-    GroupingExpression(std::unique_ptr<Expression> expr
-    ) :
-        expr(std::move(expr))
-    {}
+struct GroupingExpr {
+    const std::unique_ptr<Expr> expr;
 
-    const std::unique_ptr<Expression> expr;
+    GroupingExpr(std::unique_ptr<Expr> expr)
+    :
+        expr{std::move(expr)}
+    {}
 };
 
-class LiteralExpression : public Expression
-{
-public:
-    LiteralExpression(Literal&& literal
-    ) :
-        literal(std::move(literal))
-    {}
-
+struct LiteralExpr {
     const Literal literal;
-};
 
-class UnaryExpression : public Expression
-{
-public:
-    UnaryExpression(Token&& op,
-                    std::unique_ptr<Expression> expr
-    ) :
-        op(std::move(op)),
-        expr(std::move(expr))
+    LiteralExpr(Literal&& literal)
+    :
+        literal{std::move(literal)}
     {}
-
-    const Token op;
-    const std::unique_ptr<Expression> expr;
 };
 
-inline Expression::~Expression() {}
+struct UnaryExpr {
+    const Token op;
+    const std::unique_ptr<Expr> right;
+
+    UnaryExpr(Token&& op,
+              std::unique_ptr<Expr> right)
+    :
+        op{std::move(op)},
+        right{std::move(right)}
+    {}
+};
 }
 
 #endif // !defined(EXPRESSION_HPP)
